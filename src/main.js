dc79e4aa4f3c74283279d9cddf441e7630054f25
John-2TB <johnkoto2tb@gmail.com>
2026-07-23 00:05:35 +0100

    Implemented the About section

--- src/main.js
+++ src/main.js
@@ -146,3 +146,29 @@ cocktailLeafTimeline.from('#c-left-leaf', {
   x: 100,
   y: 100
 });
+
+// About Section Animation
+const aboutSplitText = SplitText.create('#about h2', {
+  type: 'words'
+});
+
+const scrollTimeline = gsap.timeline({
+  scrollTrigger: {
+    trigger: '#about',
+    start: 'top center',
+  }
+});
+
+scrollTimeline.from(aboutSplitText.words, {
+  opacity: 0,
+  duration: 1,
+  yPercent: 100,
+  ease: 'expo.out',
+  stagger: 0.02
+})
+.from('.top-grid div, .bottom-grid div', {
+  opacity: 0,
+  duration: 1,
+  ease: 'power1.inOut',
+  stagger: 0.04
+}, '-=0.5')
